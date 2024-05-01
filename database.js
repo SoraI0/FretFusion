5b0719c91393c3e50a1d2625e3264e9af59a921b
SoraI0 <romamogir@gmail.com>
2024-05-01 22:57:42 +0300

    багато чого...

---
+++ database.js
@@ -0,0 +1,83 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD504MXz-uzsfI_r7SlLeTMAkVreJBWl_M",
+  authDomain: "fretfusion-b00dd.firebaseapp.com",
+  projectId: "fretfusion-b00dd",
+  storageBucket: "fretfusion-b00dd.appspot.com",
+  messagingSenderId: "902263446279",
+  appId: "1:902263446279:web:422ff92221736e90c643a2"
+};
+
+const app = initializeApp(firebaseConfig);
+import {getDatabase, ref, child, get, set, update, remove} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
+
+const firebase = getDatabase();
+
+let songName = document.querySelector('#songName')
+let chordInput = document.querySelector('#chordInput')
+let textInput = document.querySelector('#textInput')
+let nextInput = document.querySelector('#nextInput')
+let endInput = document.querySelector('#endInput')
+let rowCount = 1 
+
+function addData() {
+    set(ref(firebase, 'Songs/'+ songName.value + '/' + rowCount), {
+        chord: chordInput.value,
+        text: textInput.value
+    }).then(()=>{
+        console.log('Успішно збережено');
+        rowCount++
+        chordInput.value = ''
+        textInput.value = ''
+    }).catch(()=>{
+        alert('Не збережено')
+        console.log('error');
+    })
+}
+
+nextInput.addEventListener('click', addData)
+    
+// function updateData() {
+//     update(ref(firebase, songName + chordInput.value), {
+//         chord: textInput
+//     }).then(()=>{
+//         alert('Успішно онолено')
+//     }).catch(()=>{
+//         alert('Не оновлено')
+//         console.log('error');
+//     })
+// }
+
+// function deleteData() {
+//     remove(ref(firebase, songName + chordInput.value), {
+//         chord: textInput
+//     }).then(()=>{
+//         alert('Успішно збережено')
+//     }).catch(()=>{
+//         alert('Не збережено')
+//         console.log('error');
+//     })
+// }
+
+// function retData() {
+//     const firebaseRef = ref(firebase)
+//     get(child(firebaseRef, 'Songs/'+ songName.value + '/' + rowCount)).then((snapshot)=>{
+//         if(snapshot.exists()) {
+//             chordInput.value = snapshot.val().chord
+//             textInput.value = snapshot.val().text
+//         }
+//         else {
+//             alert('ти лох')
+//         }
+//     }).catch(()=>{
+//         alert('Не виконано')
+//         console.log('error');
+//     })
+// }
+
+// retData()
+
+// const songList = document.querySelectorAll('.song')
+// console.log(songList); 
--- tuner.js
+++ tuner.js
@@ -1,81 +1,93 @@
 import autoCorrelate from "./autocorrelate.js";
 
-const audioCtx = new window.AudioContext();
-const freqDisplay = document.querySelector('#freq')
-const scale = document.querySelectorAll('.tuner__scale-point')
-console.log(scale.length);
-let analyserNode = audioCtx.createAnalyser();
 
-const noteStrings = [
-  "C",
-  "C#",
-  "D",
-  "D#",
-  "E",
-  "F",
-  "F#",
-  "G",
-  "G#",
-  "A",
-  "A#",
-  "B",
-];
 
-function getNoteFromPitchFrequency(freq) {
-  return Math.round(12 * (Math.log(freq / 440) / Math.log(2))) + 69;
-}
-
-function getPitchFrequencyFromNote(note) {
-  return 440 * Math.pow(2, (note - 69) / 12);
-}
-
-function centsOffPitch(frequencyPlayed, correctFrequency) {
-  return Math.floor(
-    (1200 * Math.log(frequencyPlayed / correctFrequency)) / Math.log(2)
-  );
-}
-
-async function setupMic() {
-  const mic = await navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: false,
-  });
-  return mic;
-}
-
-async function start() {
-  let tuningValueInput;
-  const buffer = new Float32Array(analyserNode.fftSize);
-  const mediaStream = await setupMic();
-  const mediaSource = audioCtx.createMediaStreamSource(mediaStream);
-  mediaSource.connect(analyserNode);
-  analyserNode.getFloatTimeDomainData(buffer);
-  if (audioCtx.state === "suspended") {
-    audioCtx.resume();
+  const audioCtx = new window.AudioContext();
+  const freqDisplay = document.querySelectorAll('#freq .playing-note')
+  const scale = document.querySelectorAll('.tuner__scale-point')
+  console.log(scale.length);
+  let analyserNode = audioCtx.createAnalyser();
+  
+  
+  const noteStrings = [
+    "C",
+    "C#",
+    "D",
+    "D#",
+    "E",
+    "F",
+    "F#",
+    "G",
+    "G#",
+    "A",
+    "A#",
+    "B",
+  ];
+  
+  function getNoteFromPitchFrequency(freq) {
+    return Math.round(12 * (Math.log(freq / 440) / Math.log(2))) + 69;
   }
-
-  function getSoundData() {
+  
+  function getPitchFrequencyFromNote(note) {
+    return 440 * Math.pow(2, (note - 69) / 12);
+  }
+  
+  function centsOffPitch(frequencyPlayed, correctFrequency) {
+    return Math.floor(
+      (1200 * Math.log(frequencyPlayed / correctFrequency)) / Math.log(2)
+    );
+  }
+  
+  async function setupMic() {
+    const mic = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: false,
+    });
+    return mic;
+  }
+  
+  async function start() {
+    const buffer = new Float32Array(analyserNode.fftSize);
+    const mediaStream = await setupMic();
+    const mediaSource = audioCtx.createMediaStreamSource(mediaStream);
+    mediaSource.connect(analyserNode);
     analyserNode.getFloatTimeDomainData(buffer);
-    const frequency = autoCorrelate(buffer, audioCtx.sampleRate);
-    freqDisplay.textContent = frequency.toFixed(0)
-
-    if (frequency > -1) {
-      const midiPitch = getNoteFromPitchFrequency(frequency);
-      console.log(midiPitch);
-      const playingNote = noteStrings[midiPitch % 12];
-      document
-        .getElementById("playing-note")
-        .replaceChildren(document.createTextNode(playingNote));
-      const hzOffPitch = centsOffPitch(
-        frequency,
-        getPitchFrequencyFromNote(midiPitch)
-      );
+    if (audioCtx.state === "suspended") {
+      audioCtx.resume();
     }
-    return frequency
-  }
-  
-  
-  setInterval(getSoundData, 10);
-}
 
-start();
+    
+
+    function getSoundData() {
+      analyserNode.getFloatTimeDomainData(buffer);
+      const frequency = autoCorrelate(buffer, audioCtx.sampleRate);
+      freqDisplay.textContent = frequency.toFixed(0)
+      let nName
+      if (frequency > -1) {
+        const midiPitch = getNoteFromPitchFrequency(frequency);
+        const playingNote = noteStrings[midiPitch % 12];
+        const needNote = document.querySelector('.play-section__chord')
+        nName = playingNote;
+        console.log(nName);
+        document
+          .getElementById("playing-note")
+          .replaceChildren(document.createTextNode(playingNote));
+
+          if (playingNote === needNote.textContent) {
+            setTimeout(()=>{
+              needNote.style.color = 'green'
+              setTimeout(()=>{
+                needNote.style.color = 'white'
+                needNote.textContent = 'F'
+              }, 1000)
+            }, 1000)
+            
+            
+          }
+      } 
+    }
+    
+    setInterval(getSoundData, 10);
+
+  }
+start()
